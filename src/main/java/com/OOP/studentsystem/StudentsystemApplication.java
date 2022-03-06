73ed3f2b50332766c6a26dda23297cbc9f9c2c4f
Maaruni Pandithurai <maarunipandithurai@maarunis-mbp-2.lan>
2022-03-06 19:21:13 +0800

    fileHandling put outside

--- src/main/java/com/OOP/studentsystem/StudentsystemApplication.java
+++ src/main/java/com/OOP/studentsystem/StudentsystemApplication.java
@@ -1,14 +1,14 @@
 package com.OOP.studentsystem;
 
-import com.OOP.studentsystem.fileHandling.FileStorageProperties;
+//import com.OOP.studentsystem.fileHandling.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({
-		FileStorageProperties.class
-})
+//@EnableConfigurationProperties({
+//		FileStorageProperties.class
+//})
 public class StudentsystemApplication {
 
 	public static void main(String[] args) {
