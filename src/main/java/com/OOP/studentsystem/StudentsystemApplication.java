052b408c57cd3851b71def261af53ee9e5e9a259
Shawntwj <shawntwj20@gmail.com>
2022-03-06 19:06:09 +0800

    add

---
+++ src/main/java/com/OOP/studentsystem/StudentsystemApplication.java
@@ -0,0 +1,18 @@
+package com.OOP.studentsystem;
+
+import com.OOP.studentsystem.fileHandling.FileStorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
+public class StudentsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentsystemApplication.class, args);
+	}
+
+}
